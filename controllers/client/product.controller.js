f12bcb9f1fd41b8f94b59f6a1239156fb68aec04
namhai1810 <nnh18102003@gmail.com>
2024-01-27 22:49:53 +0700

    Làm trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,16 @@
-module.exports .index=(req, res) => {
+const Product = require("../../models/product.model")
+module.exports.index= async (req, res) => {
+    const products = await Product.find({
+        status:"active",
+        deleted:"false"
+    });
+    const newProducts = products.map((item) =>{
+        item.priceNew = (item.price * (100 -item.discountPercentage)/100).toFixed(0);
+        return item;
+    })
+
     res.render('clients/pages/products/index', {
         pageTitle:"Danh sach san pham",
-        
+        products: newProducts
     });
 }
