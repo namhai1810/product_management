57a1801a773753f66353af31cf91653083d89bc6
namhai1810 <nnh18102003@gmail.com>
2024-01-27 17:25:30 +0700

    nhúng bootstrap với file tĩnh

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const route = require('./routes/client/index.route');
 require("dotenv").config()
-
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static("public"));
 app.set("views","./views");
 app.set("view engine","pug");
 
