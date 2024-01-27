bdb5c7949e091ea8612e0e6972d589f47d5e8915
namhai1810 <nnh18102003@gmail.com>
2024-01-27 22:27:46 +0700

    Them mongoose và thêm config

---
+++ models/product.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const productSchema = new mongoose.Schema(
+    {
+        title:String,
+        description: String,
+        price: Number,
+        discountPercentage: Number,
+        stock: Number,
+        thumbnail: String,
+        status: String,
+        position: Number,
+        deleted: Boolean
+    }
+);
+// dat ten model la product
+// products la ten collection trong database
+const Product = mongoose.model('Product', productSchema,"products");
+
+module.exports = Product
