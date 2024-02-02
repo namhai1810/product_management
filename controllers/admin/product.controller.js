34fb5a0dc244b6552373636b99f3f0b6038c40d0
namhai1810 <nnh18102003@gmail.com>
2024-02-02 21:33:35 +0700

    tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -8,7 +8,7 @@ module.exports.index = async (req,res) => {
             name:"Tất cả",
             status:"",
             class: "",
-        },
+        }, 
         {
             name:"Hoạt động",
             status:"active",
@@ -37,11 +37,19 @@ module.exports.index = async (req,res) => {
     if(req.query.status){
         find.status = req.query.status
     }
+
+    let keyword = "";
+    if(req.query.keyword){
+        keyword = req.query.keyword;
+        const regex = new RegExp(keyword, "i");
+        find.title = regex;
+    }
     const products = await(Product.find(find));
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     });
 } 
