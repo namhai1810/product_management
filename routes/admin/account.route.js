f56d1f782f8cceaca229770bf671a849cab00c40
namhai1810 <nnh18102003@gmail.com>
2024-02-24 16:01:53 +0700

    Làm trang sửa tài khoản

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -17,7 +17,15 @@ router.post(
   upload.single("thumbnail"),
   uploadCloud.upload,
   validate.createPost,
-  controller.createPost,
+  controller.createPost
 );
+router.get("/edit/:id", controller.edit);
 
+router.patch(
+  "/edit/:id",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.editPatch,
+  controller.editPatch
+);
 module.exports = router;
