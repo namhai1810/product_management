b716be75c25ae450e9bcc5873a7274aeb31ae37d
namhai1810 <nnh18102003@gmail.com>
2024-02-25 22:57:45 +0700

    Thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,7 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
+
 
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
@@ -10,10 +12,17 @@ module.exports.requireAuth = async (req, res, next) => {
   const user = await Account.findOne({
     token: token,
     deleted: false,
-  });
+  }).select("-password");
   if (!user) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     return;
   }
+  const role = await Role.findOne({
+    _id: user.role_id,
+    deleted: false,
+  }).select("title permissions");
+  res.locals.user = user;
+  res.locals.role = role;
+  
   next();
 };
