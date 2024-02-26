f1014f6608cc948e9d04fa17508ed424d060ad87
namhai1810 <nnh18102003@gmail.com>
2024-02-26 22:42:39 +0700

    tính năng chỉnh sửa thông tin cá nhân bên admin

--- controllers/admin/myAccount.controller.js
+++ controllers/admin/myAccount.controller.js
@@ -1,6 +1,39 @@
+const Account = require("../../models/account.model")
+
 // [GET] /admin/my-account
 module.exports.index = (req,res) => {
   res.render("admin/pages/my-account/index", {
       pageTitle: "Thông tin cá nhân",
   });
+} 
+
+// [GET] /admin/my-account/edit
+module.exports.edit = (req,res) => {
+
+  res.render("admin/pages/my-account/edit", {
+      pageTitle: "Thông tin cá nhân",
+  });
+} 
+
+// [PATCH] /admin/my-account/edit
+module.exports.editPatch = async (req,res) => {
+  
+  const emailExist = await Account.findOne({
+    _id: {$ne: res.locals.user.id},
+    email: req.body.email,
+    deleted: false,
+  });
+  if(emailExist){
+    req.flash("error", "Email đã tồn tại!");
+    res.redirect("back");
+    return;
+  }
+
+  if(req.body.password){
+    req.body.password = md5(req.body.password);
+  }else{
+    delete req.body.password;
+  }
+  res.redirect("back");
+
 } 
--- routes/admin/myAccount.route.js
+++ routes/admin/myAccount.route.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const multer = require("multer");
+const upload = multer();
 
 const controller = require("../../controllers/admin/myAccount.controller");
 
 router.get("/", controller.index);
-
-
+router.get("/edit", controller.edit);
+router.patch(
+  "/edit",
+  upload.single("avatar"),
+  uploadCloud.upload,
+  controller.editPatch
+);
 
 module.exports = router;
