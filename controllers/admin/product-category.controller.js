46d16edf1707696e63bf0a635d428be05a085375
namhai1810 <nnh18102003@gmail.com>
2024-02-28 21:15:14 +0700

    fix lỗi phần trước

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -32,7 +32,7 @@ module.exports.create = async (req, res) => {
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
   const permissions = res.locals.role.permissions;
-  if (permissions.include("products-category_create")) {
+  if (permissions.includes("products-category_create")) {
     if (req.body.position == "") {
       const count = await ProductCategory.countDocuments();
       req.body.position = count + 1;
