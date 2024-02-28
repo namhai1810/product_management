5c0adc03bf4aaf5e44f9ca0fd3f1eef5e495daf4
namhai1810 <nnh18102003@gmail.com>
2024-02-29 00:55:14 +0700

    lấy ra sản phẩm nổi bật và đẩy ra giao diện

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,10 +1,19 @@
 const ProductCategory = require("../../models/product-category.model");
+const Product = require("../../models/product.model");
 const createTreeHelpers = require("../../helpers/createTree");
+const productHelpers = require("../../helpers/product");
 // [GET] /
 module.exports.index = async (req, res) => {
- 
+  // Lấy ra các sản phẩm nổi bật
+  const find = {
+    featured: "1",
+    status: "active",
+    deleted: false,
+  }
+  const productsFeatured = await Product.find(find).sort({position:"desc"}).limit(6);
+  const products = productHelpers.priceNewProducts(productsFeatured);
   res.render("clients/pages/home/index", {
     pageTitle: "Trang chủ",
-
+    products: products
   });
 };
