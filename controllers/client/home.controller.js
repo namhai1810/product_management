afdc883014a0caa0d8a9f54905b3b54ddcf2f305
namhai1810 <nnh18102003@gmail.com>
2024-02-29 01:01:12 +0700

    Hiển thị sản phâm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -10,10 +10,22 @@ module.exports.index = async (req, res) => {
     status: "active",
     deleted: false,
   }
+  //lấy ra sản phẩm nổi bật
   const productsFeatured = await Product.find(find).sort({position:"desc"}).limit(6);
-  const products = productHelpers.priceNewProducts(productsFeatured);
+  const newProductsFeatured = productHelpers.priceNewProducts(productsFeatured);
+  //kết thúc lấy ra sản phẩm nổi bật
+
+  // Hiển thị danh sách sản phâm mới nhất
+  const newProductsNew = await Product.find({
+    status: "active",
+    deleted:false,
+  }).sort({position: "desc"}).limit(6);
+
+  // Hết hiển thị danh sách sản phâm mới nhất
+
   res.render("clients/pages/home/index", {
     pageTitle: "Trang chủ",
-    products: products
+    productsFeatured: newProductsFeatured,
+    newProductsNew: newProductsNew
   });
 };
