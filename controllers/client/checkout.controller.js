95a6c915a5d5436ba40b47b61e10060ac77d0849
namhai1810 <nnh18102003@gmail.com>
2024-03-10 21:06:53 +0700

    Lấy data được gửi lên lưu vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,8 +1,8 @@
-const Cart = require("../../models/cart.model")
-const Product = require("../../models/product.model")
+const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
 const productHelper = require("../../helpers/product");
-
-// [GET] /
+const Order = require("../../models/order.model");
+// [GET] /checkout/
 module.exports.index = async (req, res) => {
   const cartId = req.cookies.cartId;
   const cart = await Cart.findOne({
@@ -20,6 +20,45 @@ module.exports.index = async (req, res) => {
   }
   res.render("clients/pages/checkout/index", {
     pageTitle: "Đặt hàng",
-    cartDetail: cart
+    cartDetail: cart,
   });
+};
+
+//[POST] /checkout/order
+module.exports.order = async (req, res) => {
+  const cart_id = req.cookies.cartId;
+  const userInfo = req.body;
+  const cart = await Cart.findOne({
+    _id: cart_id,
+  });
+  let products = [];
+  for (const item of cart.products) {
+    const productInfo = await Product.findOne({
+      _id: item.product_id,
+    });
+    const product = {
+      product_id: item.product_id,
+      price: productInfo.price,
+      discountPercentage: productInfo.discountPercentage,
+      quantity: item.quantity,
+    };
+    products.push(product);
+  }
+  const objectOrder = {
+    cart_id: cart_id,
+    userInfo: userInfo,
+    products: products,
+  };
+  const order = new Order(objectOrder);
+  await order.save();
+
+  await Cart.updateOne(
+    {
+      _id: cart_id,
+    },
+    {
+      products: [],
+    }
+  );
+  res.send(`/checkout/success/${order.id}`);
 };
