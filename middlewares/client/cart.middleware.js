9f06e093673b690a20a105511267730bcfc6cf43
namhai1810 <nnh18102003@gmail.com>
2024-03-10 11:06:06 +0700

    Thêm minicart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -9,7 +9,11 @@ module.exports.cartId = async (req,res, next) => {
     });
   }
   else{
-
+    const cart  = await Cart.findOne({
+      _id: req.cookies.cartId,
+    });
+    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity,0);
+    res.locals.miniCart = cart;
   }
 
   next();
