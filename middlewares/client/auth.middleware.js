6d5e3162cd5dbf4892f1337b699b3bcb98442490
namhai1810 <nnh18102003@gmail.com>
2024-03-30 17:12:58 +0700

    fix bug nhỏ hiển thị thông tin khách hàng

--- middlewares/client/auth.middleware.js
+++ middlewares/client/auth.middleware.js
@@ -1,20 +1,21 @@
-const User = require("../../models/account.model");
-
+const User = require("../../models/user.model");
 
 module.exports.requireAuth = async (req, res, next) => {
   const tokenUser = req.cookies.tokenUser;
+  console.log(tokenUser);
   if (!tokenUser) {
     res.redirect(`/user/login`);
     return;
   }
-  const user = await Userr.findOne({
+  const user = await User.findOne({
     tokenUser: tokenUser,
     deleted: false,
   }).select("-password");
+  console.log(user);
   if (!user) {
     res.redirect(`/user/login`);
     return;
   }
-  
+
   next();
 };
